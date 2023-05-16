45d088c2d4787d5893fb02117da7f7eb1dd1bc14
ilikejupiter <86194083+ilikejupiter@users.noreply.github.com>
2023-05-17 01:11:02 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -62,7 +62,7 @@ export default {
       // Define a list of email addresses and their corresponding chatIds and forward email ids
       const emailList = [
         { to: "example@gmail.com", chatId: 5071059420, fMailid: "example@example.com", toForward: false }, //only sends notification as its false
-        { to: "example1@example.com", chatId: 5071059420, fMailid: "example3@example.com", toForward: true }, //sends & forward
+        { to: "akunmurah47@gmail.com.com", chatId: 5071059420, fMailid: "gamingcorner48@gmail.com.com", toForward: true }, //sends & forward
         { to: "example2@example.com", chatId: 5071059420, fMailid: "example4@example.com", toForward: false },
       ];
 
